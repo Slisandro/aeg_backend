cd9fc0797e4fc24fcfbf760fbf41c084f240347f
Lisandro <salvareschilisandro0@gmail.com>
2024-03-18 15:05:12 -0300

    update template

--- src/modules/constancies/routes/index.ts
+++ src/modules/constancies/routes/index.ts
@@ -15,22 +15,22 @@ import { ObjectId } from 'mongodb';
 
 const router = Express.Router();
 
-// const createFilesFolder = () => {
-//     try {
-//         fs.mkdir(path.join(__dirname, "../files"), (err) => {
-//             if (err) {
-//                 console.error("Error creando directorio:", err);
-//                 return;
-//             }
+const createFilesFolder = () => {
+    try {
+        fs.mkdir(path.join(__dirname, "../files"), (err) => {
+            if (err) {
+                console.error("Error creando directorio:", err);
+                return;
+            }
     
-//             console.debug("Directorio creado exitosamente");
-//         });
-//     } catch(e) {
-//         console.debug("Carpeta creada")
-//     }
-// };
+            console.debug("Directorio creado exitosamente");
+        });
+    } catch(e) {
+        console.debug("Carpeta creada")
+    }
+};
 
-// createFilesFolder();
+createFilesFolder();
 
 // get all files
 router.get("/all", async (req: Request, res: Response) => {

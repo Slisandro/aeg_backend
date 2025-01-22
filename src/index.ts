f9e27b52c239c08a55e70b675841cd405ac1af3f
Slisandro <salvareschilisandro0@gmail.com>
2025-01-22 17:17:06 -0300

    change config cors

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://aeg-frontend.vercel.app",
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     credentials: true
 }));
